53b480e8346061023d460782f7230d807f48c950
Moses C. Nah <61994757+mosesnah-shared@users.noreply.github.com>
2021-10-26 19:22:41 -0400

    Add files via upload

--- scripts/my_init_code.py
+++ scripts/my_init_code.py
@@ -15,8 +15,10 @@ import baxter_interface
 import numpy        as np
 import std_msgs.msg as msg 
 import threading
+import sys
 
 from   std_msgs.msg       import UInt16, Empty, String
+from   sensor_msgs.msg    import JointState
 from   baxter_interface   import CHECK_VERSION
 from   numpy.linalg       import inv, pinv
 from   baxter_pykdl       import baxter_kinematics
@@ -31,10 +33,72 @@ from my_constants import Constants as C
 # [BACKUP]
 # import math
 # from tf.transformations import (euler_from_quaternion, quaternion_from_euler)
- 
+
+# [REF] https://stackoverflow.com/questions/14906764/how-to-redirect-stdout-to-both-file-and-console-with-scripting
+class Logger(object):
+    def __init__(self, record_data = False):
+        self.record_data = record_data
+        self.terminal    = sys.stdout
+
+        if record_data == True:  
+            time_now         = time.strftime( '%Y_%m_%d-%H_%M' )                 # Redirecting the stdout to file
+            self.log = open( C.SAVE_DIR + 'baxter_'  + time_now + '.txt' , 'w')
+        else:
+            self.log = None
+   
+    def write(self, message ):
+        self.terminal.write( message )
+
+        if self.log is not None:
+            self.log.write( message )  
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        pass    
+
+def callback( data, start_time ):
+    # For the JointState data, we have the following data
+    # [1] position
+    # [2] velocity
+    # [3] effort
+    # [4] name
+    # [*] etc.
+    # [REF] https://stackoverflow.com/questions/57271100/how-to-feed-the-data-obtained-from-rospy-subscriber-data-into-a-variable
+    # We are interested at the joint position data, hence check whether the given data are joint data
+    # Can add more inputs if you want, by simply concatenating them
+    # [REF] https://answers.ros.org/question/332126/passing-multiple-arguments-to-subscriber-callback-function-in-python/
+    # We will check this via finding a specific string
+    if 'torso_t0' in data.name:
+
+
+        time = ( rospy.Time.now() - start_time).to_sec()    
+        for name in C.LEFT_JOINT_NAMES:
+            # Find index of the name
+            try:
+                idx = data.name.index( name )
+                rospy.loginfo( "time = %.4f, name = %s,  value = %.5f", time, name, data.position[ idx ]  ) 
+                # if file is not None:
+                    # file.write( "time = {}, name = {},  value = {}".format( time, name, data.position[ idx ] ) )
+            except:
+                NotImplementedError( )
+
+        time = ( rospy.Time.now() - start_time).to_sec()    
+        for name in C.RIGHT_JOINT_NAMES:
+            # Find index of the name
+            try:
+                idx = data.name.index( name )
+                rospy.loginfo( "time = %.4f, name = %s,  value = %.5f", time, name, data.position[ idx ]  ) 
+                # if file is not None:
+                #     file.write( "time = {%.4f}, name = {%s},  value = {%.5f}".format( time, name, data.position[ idx ] ) )
+            except:
+                NotImplementedError( )
+
+
 class BaxterControl( object ):
 
-    def __init__( self, record_data = False ):   # Using arm_type could be C.LEFT/C.RIGHT/C.BOTH
+    def __init__( self, record_data = True ):   # Using arm_type could be C.LEFT/C.RIGHT/C.BOTH
 
         self.record_data = record_data           # Boolean, data saved 
                                                  # Just define the whole arms in the first place to simplify the code
@@ -42,6 +106,7 @@ class BaxterControl( object ):
         self.kins        = range( 2 )            # Since we have both the left/right arms
         self.grips       = range( 2 )            # Since we have both the left/right arms
 
+        self.start_time  = rospy.Time.now()
 
         # Saving the limb objects
         for idx, name in enumerate( [ "right", "left" ] ):
@@ -63,12 +128,10 @@ class BaxterControl( object ):
         # self.joint_state_pub = rospy.Publisher('robot/joint_state_publish_rate', String, queue_size = 10 )
         # self.joint_state_pub.publish( "hello" )   
 
+        # print( data.position )
+        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+        sys.stdout = Logger( record_data = record_data ) 
 
-        if self.record_data:
-            time_now = time.strftime('%Y_%m_%d-%H_%M')
-            self.file_Q  = open( C.SAVE_DIR + 'Q_MotionCon_'  + time_now + '.txt', 'w')      
-     
-    
     def robot_init( self ):
 
         # Initialization of the robot
@@ -80,29 +143,22 @@ class BaxterControl( object ):
         self.rs.enable()
 
 
+    def joint_state_listener( self ):
+        
+        # rospy.init_node( 'my_listener', anonymous = False )
+        rospy.Subscriber( "robot/joint_states", JointState, callback, self.start_time )
+
+
     def joint_impedances( self, pose1, pose2, D ):
         # Inputting the joint inputs
         # Given point
         Kq = np.array( [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ] )
         Bq = np.array( [ 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 ] )
 
-        # Filling in the trajectory using minimum-jerk trajectory 
-
-    def print_pose( self, pose ):
-
-        # Print the joint data in specific order
-        if   all( "right" in s for s in pose.keys( ) ):
-            for name in C.RIGHT_JOINT_NAMES:
-                print( "Moving " + name + " to " + str( pose[ name ] ) )
-
-        elif all(  "left" in s for s in pose.keys( ) ):
-            for name in C.LEFT_JOINT_NAMES:
-                print( "Moving " + name + " to " + str( pose[ name ] ) )
             
 
     def move2pose( self, which_arm, pose, joint_speed = 0.1 ):
 
-        self.print_pose( pose )
 
 
         # If which_arm is neither 0 nor 1, assert
@@ -119,14 +175,6 @@ class BaxterControl( object ):
 
         self.arms[ which_arm ].set_joint_position_speed( joint_speed )
         self.arms[ which_arm ].move_to_joint_positions( pose )            
-
-    def print_q( self, t ):
-        q_current_dict = self.arm.joint_angles()
-        
-        if self.arm_type == RIGHT :
-            self.file_Q.write( str( t ) + " right " + ' ' + str(q_current_dict['right_s0']) + ' ' + str(q_current_dict['right_s1']) + ' ' + str(q_current_dict['right_e0']) + ' ' + str(q_current_dict['right_e1']) + ' '+ str(q_current_dict['right_w0']) + ' ' + str(q_current_dict['right_w1']) + ' ' + str(q_current_dict['right_w2']) + '\n')
-        elif self.arm_type == LEFT :
-            self.file_Q.write( str( t ) + " left " ' ' + str(q_current_dict['left_s0']) + ' ' + str(q_current_dict['left_s1']) + ' ' + str(q_current_dict['left_e0']) + ' ' + str(q_current_dict['left_e1']) + ' '+ str(q_current_dict['left_w0']) + ' ' + str(q_current_dict['left_w1']) + ' ' + str(q_current_dict['left_w2']) + '\n' )
 
 
     def grip_command( self ):
@@ -151,8 +199,11 @@ def main():
     ctrl = BaxterControl(  )
     rospy.on_shutdown( ctrl.clean_shutdown )
 
-    ctrl.move2pose( C.RIGHT, C.GRASP_POSE_RIGHT, joint_speed = 0.1 )
-    ctrl.move2pose( C.LEFT,  C.GRASP_POSE_LEFT , joint_speed = 0.1 )
+    ctrl.joint_state_listener( )
+
+    ctrl.move2pose( C.RIGHT, C.REST_POSE_RIGHT,  joint_speed = 1.0 )
+    ctrl.move2pose( C.RIGHT, C.GRASP_POSE_RIGHT, joint_speed = 1.0 )
+    # ctrl.move2pose( C.LEFT,  C.GRASP_POSE_LEFT , joint_speed = 0.3 )
 
     # ctrl.print_joints( )
     # ctrl.grip_command( )
